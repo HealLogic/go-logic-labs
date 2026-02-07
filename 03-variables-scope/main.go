2c4373540e46864b4977f1e19fecec84dca244ee
HealLogic <heallogic.dev@gmail.com>
2026-02-07 18:50:53 +0800

    feat: implement interactive console prompts for math game

--- 03-variables-scope/main.go
+++ 03-variables-scope/main.go
@@ -1,19 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+const prompt = "and press ENTER when ready."
 
 func main() {
-	//one way - declare, then assign (two steps)
-	var firstNumber int
-	firstNumber = 1
-
-	//another way, declare type and name and assign value
+	var firstNumber = 2
 	var secondNumber = 5
+	var subtraction = 7
+	// var answer int
 
-	//one step variable :  declare name, assign value and let go figure out the type
-	otherNumber := 10
+	reader := bufio.NewReader(os.Stdin)
+	
+	// display a welcome/instruction
+	fmt.Println("Guess the Number Game")
+	fmt.Println("---------------------")
+	fmt.Println("")
 
-	fmt.Println("1 :", firstNumber)
-	fmt.Println("2 :", secondNumber)
-	fmt.Println("3 :", otherNumber)
+	fmt.Println("Think of a number between 1 and 10", prompt)
+	reader.ReadString('\n')
+
+	//take  them through the games
+	fmt.Println("Multiply your number by",firstNumber,prompt)
+	reader.ReadString('\n')
+
+	fmt.Println("Now multiply the result by",secondNumber,prompt)
+	reader.ReadString('\n')
+
+	fmt.Println("Divide the result by the number you originally thought of",prompt)
+	reader.ReadString('\n')
+	
+	fmt.Println("Now subtract", subtraction,prompt)
+	reader.ReadString('\n')
+
+	//give them the answer
 }
