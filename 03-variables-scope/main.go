1a041f3a575842aea1d861861546ddc1661c52d6
HealLogic <heallogic.dev@gmail.com>
2026-02-08 09:50:22 +0800

    refactor: extract game logic into playGuessNumber function

--- 03-variables-scope/main.go
+++ 03-variables-scope/main.go
@@ -14,11 +14,18 @@ func main() {
 	//seed the random number generator
 	rand.Seed(time.Now().UnixNano())
 
-	var firstNumber = rand.Intn(8) + 2
-	var secondNumber = rand.Intn(8) + 2
-	var subtraction = rand.Intn(8) + 2
-	var answer int
+	//rand generates a number between 0 and whatever is passed as a parameter
+	//we add 1 to it because we want the number used to be at least 1, and not
+	//greater than 10
+	var firstNumber = rand.Intn(9) + 1
+	var secondNumber = rand.Intn(9) + 1
+	var subtraction = rand.Intn(9) + 1
+	var answer = firstNumber * secondNumber - subtraction
 
+	playGuessNumber(firstNumber,secondNumber,subtraction,answer)
+}
+
+func playGuessNumber(firstNumber,secondNumber,subtraction,answer int){
 	reader := bufio.NewReader(os.Stdin)
 	
 	// display a welcome/instruction
@@ -43,6 +50,5 @@ func main() {
 	reader.ReadString('\n')
 
 	//give them the answer
-	answer = firstNumber * secondNumber - subtraction
 	fmt.Println("The answer is", answer)
 }
